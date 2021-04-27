06572f3ea5ada5ca553d420b0b28c9f5c4044246
LeoToti <Londra27?>
2021-04-27 15:58:56 +0200

    React

---
+++ first-one/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar , Nav, NavDropdown, Button, Form, FormControl } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="">
+     <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">About</Nav.Link>
+      <Nav.Link href="#link">Browse</Nav.Link>
+      
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+    </div>
+  );
+}
+
+export default App;
